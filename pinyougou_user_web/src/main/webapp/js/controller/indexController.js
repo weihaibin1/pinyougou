fd16f23614f14aed87ae2b5b9b0558ac7ba4de93
weihaibin <15268926537@163.com>
2018-12-27 15:54:50 +0800

    用户中心模块展示

--- pinyougou_user_web/src/main/webapp/js/controller/indexController.js
+++ pinyougou_user_web/src/main/webapp/js/controller/indexController.js
@@ -1,19 +1,14 @@
-app.controller("indexController",function ($scope,$controller,contentService) {
+app.controller("indexController",function ($scope,$controller,loginService) {
 
-    //控制器继承代码
-    $controller("baseController",{$scope:$scope});
+    //控制器继承代码  参数一：继承的父控制器名称   参数二：固定写法，共享$scope对象
+    $controller("baseController",{$scope:$scope})
 
-    //根据广告分类id查询广告列表数据
-    $scope.findByCategoryId=function (categoryId) {
-        contentService.findByCategoryId(categoryId).success(function (response) {
-            //定义广告列表接受返回的数据
-            $scope.contentList=response;
+
+    //获取用户名
+    $scope.getName=function () {
+        //response接收响应结果
+        loginService.getName().success(function (response) {
+            $scope.loginName=response.loginName;
         })
-    }
-
-    //门户网站搜索功能    与搜索模块对接
-    $scope.search=function () {
-        //注意：angularjs页面传参时(路由传参)，请求参数？号前面，需要加#
-        location.href="http://search.pinyougou.com/search.html#?keywords="+$scope.keywords;
     }
 })
